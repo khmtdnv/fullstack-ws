ccac3ef921952c1f603a385524c7b26ed58c16fc
iwaeiw <marvelous.rust@yandex.ru>
2025-08-31 02:52:47 +0500

    hid secret key, updated requirements

--- fullstack-ws/main/settings/base.py
+++ fullstack-ws/main/settings/base.py
@@ -13,20 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ffu2)mapi472l+cm79a_hkdz+yvrz(%l^t!@k@3zgtv_e&kl3o"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Application definition
 
