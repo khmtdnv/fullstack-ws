04fce1b0ccc816f5f3b74abbf24d53a425417bd2
iwaeiw <marvelous.rust@yandex.ru>
2025-09-16 15:37:22 +0500

    added discount system

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 python3 -m venv env/arrayshop  
 source env/arrayshop/bin/activate
 source /home/tikkazar/learn/fullstack-ws/env/arrayshop/bin/activate
+
+
+CELERY:  в папке fullstack-ws/ -> celery -A main worker -l debug
+STRIPE:  stripe listen --forward-to localhost:8000/payment/webhook/
--- fullstack-ws/payment/tasks.py
+++ fullstack-ws/payment/tasks.py
@@ -10,12 +10,19 @@ from orders.models import Order
 @shared_task
 def payment_completed(order_id):
     order = Order.objects.get(id=order_id)
-    subject = f"Shop - Invoice no. {order_id}"
-    message = "Here's your invoice for your recent purchase."
-    email = EmailMessage(subject, message, "admin@shop.com", [order.email])
-    html = render_to_string("orders/order/pdf.html", {"order": order})
+    subject = f'Array shop - Invoice no. {order.id}'
+    message = 'Please, find attached the invoice for your recent purchase.'
+    email = EmailMessage(subject,
+                         message,
+                         'admin@shop.com',
+                         [order.email])
+    html = render_to_string('orders/order/pdf.html',
+                            {'order': order})
     out = BytesIO()
-    stylesheet=[weasyprint.CSS(settings.STATIC_ROOT / "css/pdf.css")]
-    weasyprint.HTML(string=html).write_pdf(out, stylesheets=stylesheets)
-    email.attach(f"order_{order.id}.pdf", out.getvalue(), "application/pdf")
+    stylesheets=[weasyprint.CSS(settings.STATIC_ROOT / 'css/pdf.css')]
+    weasyprint.HTML(string=html).write_pdf(out,
+                                           stylesheets=stylesheets)
+    email.attach(f'order_{order.id}.pdf',
+                 out.getvalue(),
+                 'application/pdf')
     email.send()
